5f21978b30c16d05084e5ed3b877a610f20ac6c6
kasitah006 <okhavakali@gmail.com>
2025-02-05 10:10:35 +0300

    version one

---
+++ controlstatement.py
@@ -0,0 +1,30 @@
+temperature = 23
+
+if temperature < 25 :
+    print("it is too cold")
+else:
+    print("it is too hot")
+
+#A program that returns the largest number
+num1 = 34
+num2 = 12
+num3 = 45
+
+if num1 > num2 and num1 > num3:
+    print(num1, "is the largest")
+elif num2 > num1 and num2 > num3:
+    print(num2, "is the largest")
+else:
+    print(num3, "is the largest number")
+
+#A program to check whether a number is even or odd
+number = 0
+if number == 0:
+    print(number, "is a neutral number")
+elif number % 2 == 0:
+    print(number, "is an even number")
+else:
+    print(number, "is an odd number")
+
+
+
---
+++ loops.py
@@ -0,0 +1,11 @@
+# While loop
+ #Increment
+number = 105
+while number <= 110  :
+    print(number)
+    number += 1
+#Decrement
+num1 = 30
+while num1 >=25 :
+    print( num1)
+    num1 -= 1
