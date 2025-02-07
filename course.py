9dca0660f9d90be3e4c53c26e06fc2890c38d972
kasitah006 <okhavakali@gmail.com>
2025-02-07 10:13:11 +0300

    version three

---
+++ course.py
@@ -0,0 +1,7 @@
+import module
+import functions
+
+module.course("john","MIT")
+functions.Employee("John",19,"Secretary",67000)
+
+
---
+++ exceptions.py
@@ -0,0 +1,17 @@
+
+try :
+    print(x)
+except:
+    print("An error occurred")
+finally:
+    print("Success")
+
+
+
+try :
+    num1 = 20
+    num2 = 0
+    print(num1 / num2)
+except :
+    print("An error occurred")
+
